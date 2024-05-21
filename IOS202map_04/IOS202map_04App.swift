e6f0dd64ebc1eed74158698d99aa5d175bf331a4
23cm0104 <23cm0104@jec.ac.jp>
2024-05-21 14:31:47 +0900

    Initial Commit

---
+++ IOS202map_04/IOS202map_04App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS202map_04App.swift
+//  IOS202map_04
+//
+//  Created by cmStudent on 2024/05/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS202map_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
