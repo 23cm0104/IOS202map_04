0c5689f27fc4d25de7edeb46978c2bfbc3e77dc3
23cm0104 <23cm0104@jec.ac.jp>
2024-05-23 05:12:37 +0900

    Modify ContentView

--- IOS202map_04/ContentView.swift
+++ IOS202map_04/ContentView.swift
@@ -10,13 +10,9 @@ import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(){
+            
         }
-        .padding()
     }
 }
 
