fb3b6be68e0eff3220c4ba144b54e1d7398cfbb4
23cm0104 <23cm0104@jec.ac.jp>
2024-05-23 05:24:24 +0900

    Modify something

---
+++ IOS202map_04/Station.swift
@@ -0,0 +1,8 @@
+//
+//  Station.swift
+//  IOS202map_04
+//
+//  Created by cmStudent on 2024/05/23.
+//
+
+import Foundation
---
+++ IOS202map_04/StationFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  StationFetcher.swift
+//  IOS202map_04
+//
+//  Created by cmStudent on 2024/05/23.
+//
+
+import Foundation
