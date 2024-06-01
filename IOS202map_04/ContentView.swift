d6f95e7aeab187a107b31ba441002a373123f10d
23cm0104 <23cm0104@jec.ac.jp>
2024-06-02 04:35:33 +0900

    Update ContentView.swift

--- IOS202map_04/ContentView.swift
+++ IOS202map_04/ContentView.swift
@@ -25,39 +25,31 @@ struct ContentView: View {
             List(fetcher.stationList) { station in
                 NavigationLink(
                     destination: {
-                        VStack {
-                            HStack {
-                                Text("最寄駅: \(station.name)")
-                                Text("前の駅: \(station.prev ?? "なし")")
-                                Text("次の駅: \(station.next ?? "なし")")
-                            }.padding(.top)
-                            Text("距離: \(station.distance)")
-                            Map() {
-                                UserAnnotation()
-                                Marker("\(station.name)", systemImage: "tram.circle", coordinate: CLLocationCoordinate2D(latitude: station.y, longitude: station.x))
-                                if let showRoute = locationManager.route?.polyline{
-                                    MapPolyline(showRoute).stroke(.blue, style: StrokeStyle(lineWidth: 6, lineCap: .round, dash: [0.5, 10]))
-                                }
-                            }.mapControls {
-                                MapUserLocationButton().mapControlVisibility(.visible)
-                                MapPitchToggle()
-                                MapCompass()
-                                MapScaleView().mapControlVisibility(.hidden)
-                            }.onAppear {
-                                let way = ways[vehicle]
-                                switch way {
-                                case .walking:
-                                    transportType = .walking
-                                case .automobile:
-                                    transportType = .automobile
-                                case .transit:
-                                    transportType = .transit
-                                    
-                                default:
-                                    transportType = .any
-                                }
-                                locationManager.getRoute(from: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), to: CLLocationCoordinate2D(latitude: station.y, longitude: station.x), transportType: transportType ?? .any)
+                        Map() {
+                            UserAnnotation()
+                            Marker("最寄駅: \(station.name)駅\n前の駅: \(station.prev ?? "なし")駅\n次の駅: \(station.next ?? "なし")駅\n距離: \(station.distance)\n郵便番号: \(station.postal)\n路線名: \(station.line)", systemImage: "tram.circle", coordinate: CLLocationCoordinate2D(latitude: station.y, longitude: station.x))
+                            if let showRoute = locationManager.route?.polyline{
+                                MapPolyline(showRoute).stroke(.blue, style: StrokeStyle(lineWidth: 6, lineCap: .round, dash: [0.5, 10]))
                             }
+                        }.mapControls {
+                            MapUserLocationButton().mapControlVisibility(.visible)
+                            MapPitchToggle()
+                            MapCompass()
+                            MapScaleView().mapControlVisibility(.hidden)
+                        }.onAppear {
+                            let way = ways[vehicle]
+                            switch way {
+                            case .walking:
+                                transportType = .walking
+                            case .automobile:
+                                transportType = .automobile
+                            case .transit:
+                                transportType = .transit
+                                
+                            default:
+                                transportType = .any
+                            }
+                            locationManager.getRoute(from: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), to: CLLocationCoordinate2D(latitude: station.y, longitude: station.x), transportType: transportType ?? .any)
                         }
                     }
                 ){
